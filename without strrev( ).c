463217daa24ef47f6a496be30d01f13c8367f097
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-02-27 16:32:42 +0530

    Create without strrev( ).c

---
+++ without strrev( ).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,m,s=0;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        m=n%10;
+        s=s*10+main;
+        n/=10;
+    }
+    printf("The reverse of the given number:%d",s);
+}
