cdf9f4541b43107b5a939dfbf47d36b2f609c1d7
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-02-28 19:03:51 +0530

    Create area triangle.c

---
+++ area triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,h,b;
+printf("Enter the triangle base:");
+scanf("%d",&b);
+printf("Enter the triangle height:");
+scanf("%d",&h);
+a=(b*h)/2;
+printf("The area of triangle:%d",a);
+}
