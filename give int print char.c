a698bac455172c5e925a55c70b93e84d3268b360
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-03-01 16:11:28 +0530

    Update and rename give int print char.c to give int print the char.c

--- give int print char.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    char a;
-    printf("Enter the number:");
-    scanf("%d",&a);
-    printf("The character is:%c",(a+64));
-    
-}
---
+++ give int print the char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    char b;
+    printf("Enter the number:");
+    scanf("%d",&b);
+    printf("The character is:%c",(b+64));
+    
+}
