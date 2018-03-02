6a466fe2a3a528a2549b83a8afb3fa51f63d904b
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-03-02 14:37:25 +0530

    Create print 2nd small num.c

---
+++ print 2nd small num.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int n,a[100],t,i,j;
+	printf("Enter the number of elements : ");
+	scanf("%d",&n);
+	printf("Enter the elements : ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	printf("The 2nd smallest element is %d.",a[1]);
+}
