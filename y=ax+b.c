d5eb42e99fe5e26f3a9f8cea9b71f58254cdb7bc
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-03-20 16:33:07 +0530

    Create y=ax+b.c

---
+++ y=ax+b.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,x,y;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    printf("Enter the value of x:");
+    scanf("%d",&x);
+    y=((a*x)+b);
+    printf("The value of y is:%d",y);
+    
+    
+}
